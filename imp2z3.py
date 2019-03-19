7767d58c68e7b8ea2817a841bacf7b735d002cb3
Kevin <kevin_ray@ku.edu>
2019-03-19 18:27:19 -0500

    first commit

---
+++ imp2z3.py
@@ -0,0 +1,28 @@
+import sys
+from imp_interp.imp_parser import *
+from imp_interp.imp_lexer import *
+
+def main(args):
+
+    if len(args) != 3:
+        print("Usage: python imp2z3.py <input filepath> <output filepath>")
+        sys.exit(1)
+
+    ast = generateAST(args[1])
+    if not ast:
+        sys.stderr.write("File could not be parsed")
+        sys.exit(1)
+    print(ast)
+
+
+def generateAST(filepath):
+    imp_text = open(filepath).read()
+    tokens = imp_lex(imp_text)
+    result = imp_parse(tokens)
+    if not result:
+        return None
+    return result.value
+
+
+if __name__ == '__main__':
+    main(sys.argv)
